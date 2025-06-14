d51e8dfbddfea78976e1f6fe0cef5ead8d08058d
Istehsungoglobal <istehsun@goglobalbd.net>
2025-06-14 18:05:38 +0600

    Update navbar.blade.php

--- resources/views/layouts/fontendpart/navbar.blade.php
+++ resources/views/layouts/fontendpart/navbar.blade.php
@@ -4,7 +4,7 @@
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container">
                 <div class="navbar-logo" >
-                    <img src="{{asset('assets/images/funnel/Logo.png')}}" alt="Logo">
+                    <img style="height: 50px;" src="{{asset('assets/images/funnel/Logo.png')}}" alt="Logo">
                 </div>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
                 </button>
